6f26f92fe85e30adf5e9ff65e15817883766adcc
Emir <78050550+emir1031@users.noreply.github.com>
2021-05-29 21:22:28 +0200

    Add files via upload

---
+++ python/class_notes/2021_05_20_MODULES/first_module.py
@@ -0,0 +1,11 @@
+def my_function():
+    print("!!! inside MY_MODULE module !!!")
+    print("!!! inside second module !!!")
+    print("inside my module, my function...")
+
+def funct2(a):
+    print("!!! inside MY_MODULE module !!!")
+    print("incoming argument: ", a)
+
+def f2():
+    print("tttt")
