c4f97a64313bc4ffcd56cd280f0f25a431ee31b5
Emir <78050550+emir1031@users.noreply.github.com>
2021-05-29 21:31:34 +0200

    Delete python/lms_answers directory

--- python/lms_answers/sum_double.py
+++
@@ -1,9 +0,0 @@
-'''Given two integer values, return their sum. 
-If the two values are the same, then return 
-double their sum.'''
-
-def sum_double(x, y):
-    if x != y:
-        return x + y
-    else:
-        return (x + y) * 2 
