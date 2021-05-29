0713da47bc035fe7e0ff3467f86d886c9029d81d
Emir <78050550+emir1031@users.noreply.github.com>
2021-05-29 21:34:17 +0200

    Add files via upload

---
+++ python/class_notes/2021_05_20_MODULES/second_module.py
@@ -0,0 +1,9 @@
+def test1():
+    print("!!! inside second module !!!")
+    print("function without paramethers!")
+def test2(firstarg):
+    print("!!! inside second module !!!")
+    print("incoming argument: ", firstarg)
+
+def f2():
+    print("qqqqqqqaaaaaaa")
