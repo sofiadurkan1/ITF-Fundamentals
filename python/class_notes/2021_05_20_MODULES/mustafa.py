dc91fc626acaba25ceb797da99d7313521079cf6
Emir <78050550+emir1031@users.noreply.github.com>
2021-05-29 20:52:54 +0200

    Delete python directory

--- python/class_notes/2021_05_20_MODULES/mustafa.py
+++
@@ -1,5 +0,0 @@
-def adam(argument):
-    print("inside MUSTAFA")
-    print("incoming argument:", argument)
-
-print("an independent print statement")
--- python/class_notes/2021_05_20_MODULES/second_module.py
+++
@@ -1,9 +0,0 @@
-def test1():
-    print("!!! inside second module !!!")
-    print("function without paramethers!")
-def test2(firstarg):
-    print("!!! inside second module !!!")
-    print("incoming argument: ", firstarg)
-
-def f2():
-    print("qqqqqqqaaaaaaa")
